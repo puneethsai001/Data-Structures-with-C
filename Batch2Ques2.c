58c90a0a4b724e14fa3a8e7960d608de6bfe7748
puneethsai001 <83299062+puneethsai001@users.noreply.github.com>
2024-01-03 16:29:54 +0400

    Update Batch2Ques2.c

--- Batch2Ques2.c
+++ Batch2Ques2.c
@@ -1,124 +1,133 @@
-//NOTE: The answer may not be 100% correct since the question is unclear
-
 #include <stdio.h>
 #define MAX_SIZE (20)
 
-typedef struct Stack{
+typedef struct Queue{
     int A[MAX_SIZE];
-    int top;
-}Stack;
+    int front;
+    int rear;
+}Queue;
 
-void initializeStack(Stack *s){
-    s->top = -1;
+void initializeQueue(Queue *q){
+    q->front = 0;
+    q->rear = -1;
 }
 
-void push(Stack *s, int value){
-    if (s->top == MAX_SIZE - 1){
-        printf("Stack Overflow");
+void enqueue(Queue *q, int value){
+    if (q->rear == MAX_SIZE - 1){
+        printf("QUEUE OVERFLOW: ");
         return;
     }
 
-    s->top = s->top + 1;
-    s->A[s->top] = value;
+    q->rear = q->rear + 1;
+    q->A[q->rear] = value;
 }
 
-int pop(Stack *s){
-    if (s->top == -1){
-        printf("Stack Underflow");
+int dequeue(Queue *q){
+
+    if (q->rear < q->front){
+        printf("NO PRINT JOBS: ");
         return 0;
     }
 
-    return s->A[s->top--];
+    int a = q->A[q->front];
+    q->front = q->front + 1;
+    return a;
 }
 
-int isEmpty(Stack *s){
-    if (s->top == -1){
-        return 1;
-    }
-
-    return 0;
-}
-
-void display(Stack *s){
-    if (s->top == -1){
-        printf("Stack Empty\n");
+void display(Queue *q){
+    if (q->front > q->rear){
+        printf("PRINTER QUEUE EMPTY\n");
         return;
     }
 
-    printf("Text Editor: ");
-    for (int i = 0; i < s->top+1; i++){
-        printf("%d ", s->A[i]);
+    printf("Printer Status: ");
+    for (int i = q->front; i <= q->rear; i++){
+        printf("%d ",q->A[i]);
     }
     printf("\n");
 }
 
+int rearValue(Queue *q){
+    return q->rear;
+}
+
+int frontValue(Queue *q){
+    return q->front;
+}
+
+
 int main(){
 
-    Stack myStack, redoStack;
-
-    initializeStack(&myStack);
-    initializeStack(&redoStack);
+    Queue myqueue;
+    initializeQueue(&myqueue);
 
     int choice, flag = 1;
-    int ele;
+    int ele, temp, invalid = 1;
+    int computers[10];
+
+    for (int i = 1; i <= 10; i++){
+        computers[i-1] = i;
+    }
 
     while (flag){
-        printf("\n1. Push an item\n");
-        printf("2. Undo previous operation\n");
-        printf("3. Redo previous operation\n");
+        
+        printf("===================== Print Operation Menu =====================\n");
+        printf("1. Add a Print Job\n");
+        printf("2. Print a Job\n");
+        printf("3. Display Job Queue Status\n");
         printf("4. Exit\n");
 
         printf("Enter Choice: ");
-        scanf("%d", &choice);
+        scanf("%d",&choice);
 
         switch (choice)
         {
         case 1:
-            printf("Enter Item: ");
+
+            printf("Enter Computer No: ");
             scanf("%d", &ele);
 
-            push(&myStack, ele);
-            initializeStack(&redoStack);
-            display(&myStack);
-            break;
+            temp = rearValue(&myqueue);
 
+            enqueue(&myqueue, ele);
+
+            if (temp == rearValue(&myqueue)){
+                printf("CANT ADD MORE JOBS\n");
+            }
+
+            else{
+                printf("ADDED SUCCESSFULLY\n");
+            }
+
+            break;
+        
         case 2:
-            ele = pop(&myStack);
+            temp = frontValue(&myqueue);
 
-            if (ele != 0){
-                push(&redoStack, ele);
-                printf("Undo Success\n");
+            dequeue(&myqueue);
+
+            if (temp == frontValue(&myqueue)){
+                printf("CANT PRINT\n");
             }
 
             else{
-                printf("\nUndo Unsuccessfull");
+                printf("PRINTED SUCCESSFULLY\n");
             }
 
-            display(&myStack);
             break;
-
+        
         case 3:
-            ele = pop(&redoStack);
-
-            if (ele != 0){
-                push(&myStack, ele);
-                printf("Redo Success\n");
-            }
-
-            else{
-                printf("\nRedo Unsuccessfull\n");
-            }
-
-            display(&myStack);
+            display(&myqueue);
             break;
         
         case 4:
             flag = 0;
             break;
-        
+
         default:
-            break;
+            printf("Invalid Input\n");
         }
     }
+
     return 0;
 }
